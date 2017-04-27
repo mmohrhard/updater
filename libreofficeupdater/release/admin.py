399654874d13614d2777c071d33471e58b5f71bb
Markus Mohrhard <markus.mohrhard@googlemail.com>
2017-04-27 02:06:14 +0200

    show partial modek objects in admin interface

--- libreofficeupdater/release/admin.py
+++ libreofficeupdater/release/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import MarFile, LanguageFile, UpdateChannel, Release
+from .models import MarFile, LanguageFile, UpdateChannel, Release, PartialUpdate, PartialLanguageUpdate
 
 admin.site.register(MarFile)
 admin.site.register(LanguageFile)
 admin.site.register(UpdateChannel)
 admin.site.register(Release)
+admin.site.register(PartialUpdate)
+admin.site.register(PartialLanguageUpdate)
